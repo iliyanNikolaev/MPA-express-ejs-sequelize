c7b98d99c8e5b5079b5689ae24d9c79814c1fb27
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-12-11 20:24:01 +0200

    remove jquery examples from repo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 app.use('/api/laptops', laptopRouter);
 
 app.listen(6161, () => console.log('server is listening on port 6161'));
